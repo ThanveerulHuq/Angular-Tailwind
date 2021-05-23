2ad605edcf751ea4574674f262740c1b087c9c08
thanveer <thanveerul.haq@nielsen.com>
2021-05-23 22:14:56 +0530

    bill calculator UI added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-v12-practice';
   units!: number;
-  billAmount!: number;
+  billAmount!: number | string;
 
   onCalculate() {
-    
-
+    if (!this.units) return;
+    if (this.units <= 100) {
+      this.billAmount = 0;
+    } else if (this.units > 100 && this.units <= 200) {
+      const calcAmnt = (this.units - 100) * 1.5;
+      this.billAmount = calcAmnt + 20;
+    } else if (this.units > 200 && this.units <= 500) {
+      const calcAmnt = (this.units - 200) * 3;
+      this.billAmount = 230 + calcAmnt;
+    } else {
+      this.billAmount = this.units * 6.6 - 1520;
+    }
+    this.billAmount = this.billAmount.toFixed(2);
   }
 }
