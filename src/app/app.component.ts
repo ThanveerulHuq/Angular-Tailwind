076a05e1861603debf207cbad4a31cd1e018ca0a
thanveer <thanveerul.haq@nielsen.com>
2021-05-23 20:12:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-v12-practice';
+  units!: number;
+  billAmount!: number;
+
+  onCalculate() {
+    
+
+  }
 }
