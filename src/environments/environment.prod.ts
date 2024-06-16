cfc9f92b37dc4f044bff3af8780cd67400d4c958
Thanveerul Huq <thanver@amazon.com>
2024-06-16 18:27:05 +0530

    prod env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseKey: 'YOUR_SUPABASE_KEY',
+  supabaseUrl: 'https://mpoeqllzybepjsoleeru.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb2VxbGx6eWJlcGpzb2xlZXJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NzE5NzAsImV4cCI6MjAzNDA0Nzk3MH0.N5EOaFgx0chUj1NOzxj8cQuumNPGnO6q5BKzu3GknEE',
 };
