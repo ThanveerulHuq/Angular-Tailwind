1e9bc95c636f1a104b289e9b92df2af3eb4008c6
Thanveerul Huq <thanver@amazon.com>
2024-06-16 18:24:01 +0530

    supabase service added

---
+++ src/app/services/supabase.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {createClient, SupabaseClient} from "@supabase/supabase-js";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+  saveBillAmount(unit: number, amount: number) {
+    return this.supabase
+      .from('bill_amounts')
+      .insert([
+        {unit, amount}
+      ]);
+  }
+
+}
